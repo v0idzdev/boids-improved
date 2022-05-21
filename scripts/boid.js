00a50f3d39dcfaf1802a18073bf3ba2b8ee5ed0c
Matthew Flegg <matthewflegg@outlook.com>
2022-05-21 12:49:29 +0000

    Added test code

--- scripts/boid.js
+++ scripts/boid.js
@@ -1,273 +1,218 @@
-const boidColors = ["#FB8C0D", "#E35E0B", "#FA3A00", "#E31D0B", "#FF1A4E"];
-
 /**
- * Models a particle emitted by a boid.
+ * Custom data type that provides an abstracted interface for working
+ * with spatial hashing/spatial subdivisions.
  */
-class Particle {
+class SpatialHash {
   /**
-   * Create a particle instance.
+   * Creates a new SpatialHash instance. Append boids to the spatial hash
+   * by calling SpatialHash.add(boid).
+   *
+   * @param {Object} params Parameters used to construct the spatial hash.
    */
-  constructor({ position: [x, y], velocity: [vx, vy], color: value }) {
-    this.position = createVector(x, y);
-    this.velocity = createVector(vx, vy);
-
-    this.color = lerpColor(
-      color(value),
-      color(random(boidColors)),
-      deltaTime / 1000
-    );
-    this.alpha = 150;
-    this.sizeDissipation = random(0.01, 0.63); // The speed at which the size decreases.
-    this.alphaDissipation = random(1.1, 8.2); // The speed at which the transparency decreases.
-    this.size = random(1.5, 9.5);
-
-    // Random direction.
-    this.position = createVector(
-      random(
-        this.position.x - this.velocity.y,
-        this.position.x + this.velocity.y
-      ),
-      random(
-        this.position.y - this.velocity.x,
-        this.position.y + this.velocity.x
-      )
-    );
+  constructor({ cellSize: size = 50 }) {
+    this.cellSize = size
+    this.hashTable = {}
   }
 
   /**
-   * Updates the particle's transparency and size values.
+   * Appends a boid to the spatial hash. Pass in the boid instance as
+   * opposed to its attributes.
+   *
+   * @param {Boid} boid The boid object to add to the hash table.
    */
-  update() {
-    this.alpha -= this.alphaDissipation;
-    this.size -= this.sizeDissipation;
-    this.color.setAlpha(this.alpha);
+  add(boid) {
+    let key = this._calculateKey(boid)
+    if (this.hashTable[key] == undefined) this.hashTable[key] = []
+    this.hashTable[key].push(boid)
   }
 
   /**
-   * Renders the particle on the canvas.
+   * Removes a boid from the spatial hash. Pass in the boid instance as
+   * opposed to its attributes.
+   *
+   * @param {Boid} boid The boid object to remove from the hash table.
    */
-  show() {
-    fill(this.color);
-    circle(this.position.x, this.position.y, this.size);
+  remove(boid) {
+    let key = this._calculateKey(boid)
+    if (this.hashTable[key] == undefined) this.hashTable[key] = []
+    delete this.hashTable[key]
   }
 
   /**
-   * Returns a boolean value indicating whether the particle is
-   * invisible or has gone outside of the bounds of the simulation.
+   * Returns an array of boids at the cell that encompasses its position
+   * co-ordinates.
+   *
+   * @param {Boid} boid The boid to return all items for in its cell.
    */
-  finished() {
-    return (
-      this.alpha < 0 ||
-      this.size < 0 ||
-      this.position.x < -width / 2 ||
-      this.position.x > width / 2 ||
-      this.position.y < -height / 2 ||
-      this.position.y > height / 2
-    );
+  get(boid) {
+    let key = this._calculateKey(boid)
+    if (this.hashTable[key] == undefined) this.hashTable[key] = []
+    return this.hashTable[key]
+  }
+
+  /**
+   * Calculates the key of a given boid within the hash table. Used as
+   * a private, internal method.
+   *
+   * @param {Boid} boid The boid to find the key of.
+   */
+  _calculateKey(boid) {
+    return `${round(boid.position.x / this.cellSize) * this.cellSize},${
+      round(boid.position.y / this.cellSize) * this.cellSize
+    }`
   }
 }
 
 /**
- * Models an agent in the flocking simulation.
+ * Represents the collection of boids being simulated. Parameters
+ * used here affect all of the boids within the simulation.
+ */
+class Flock {
+  /**
+   * Create a flock object. Change parameters passed in to alter
+   * the behaviour of all boids in the simulation.
+   *
+   * @param {Object} params The parameters used to initialise the flock.
+   */
+  constructor({
+    spatialHashCellSize: size,
+    numBoids: num,
+    boidSettings: {
+      // Controls boid parameters.
+      // Used to create new boids in the Flock constructor.
+      boidDiameter: diameterValue,
+      alignment: alignmentValue,
+      separation: separationValue,
+      cohesion: cohesionValue,
+      initialVelocityRange: {
+        min: minVelocityValue = 2, // Minimum initial velocity.
+        max: maxVelocityValue = 4, // Maximum initial velocity.
+      },
+
+      maxForce: maxForceValue,
+      maxSpeed: maxSpeedValue,
+      colorPalette: hexColorValues = [
+        // These are set to a default value.
+        "#FB8C0D",
+        "#E35E0B",
+        "#FA3A00",
+        "#E31D0B",
+        "#FF1A4E",
+      ],
+    },
+  }) {
+    this.boids = this._generateBoids()
+    this.spatialHashCellSize = size
+    this.numBoids = num
+    this.boidSettings = boidSettings
+  }
+
+  /**
+   * Renders the boids on screen on a cell-by-cell basis,
+   * using the spatial partition created for the boid simulation.
+   */
+  update() {
+    // Update the spatial hash table
+
+    // Update each cell and the boids within it
+    for (const [key, boidArray] of Object.entries(this.boids))
+      for (let i = 0; i < boidArray.length; i++) {
+        boidArray[i].update(boidArray)
+      }
+  }
+
+  /**
+   * Generates an array of boids and appends them to the spatial partition
+   * table. Generates boids randomly based on Flock parameters.
+   */
+  _generateBoids() {
+    let boids = new SpatialHash({ cellSize: this.spatialHashCellSize })
+
+    for (let i = 0; i < this.numBoids; i++)
+      boids.add(new Boid(this.boidSettings))
+
+    return boids
+  }
+}
+
+/**
+ * Represents a boid within the simulation. Can be configured
+ * by changing the parameters passed into the constructor.
  */
 class Boid {
   /**
-   * Create a boid instance.
+   * Create a boid object. In order to exhibit different behaviour,
+   * change the parameters used in this function.
+   *
+   * @param {Object} boidSettings Settings to control the boid.
    */
-  constructor() {
+  constructor({
+    boidSettings: {
+      // Controls boid parameters.
+      boidDiameter: diameterValue,
+      alignment: alignmentValue,
+      separation: separationValue,
+      cohesion: cohesionValue,
+      initialVelocityRange: {
+        min: minVelocityValue = 2, // Minimum initial velocity.
+        max: maxVelocityValue = 4, // Maximum initial velocity.
+      },
+
+      maxForce: maxForceValue,
+      maxSpeed: maxSpeedValue,
+      colorPalette: hexColorValues = [
+        // These are set to a default value.
+        "#FB8C0D",
+        "#E35E0B",
+        "#FA3A00",
+        "#E31D0B",
+        "#FF1A4E",
+      ],
+    },
+  }) {
+    // Configure basic parameters.
+    this.diameter = diameterValue
+    this.alignment = alignmentValue
+    this.separation = separationValue
+    this.cohesion = cohesionValue
+
+    // Initialise the boid's position as a random location on screen.
     this.position = createVector(
-      random(-width, width),
-      random(-height, height)
-    );
+      random(-width / 2, width / 2),
+      random(-height / 2, height / 2)
+    )
 
-    this.velocity = p5.Vector.random2D().setMag(random(2, 4));
-    this.acceleration = createVector();
-    this.maxForce = 1; //0.2;
-    this.maxSpeed = 4;
-    this.color = random(boidColors);
-    // this.particlesPerSecond = 100;
-    // this.timeSinceLastParticle = 0;
-    this.particles = [];
+    // Initialise the boid's velocity randomly between the minimum and
+    // maximum initial velocities passed in.
+    this.velocity = p5.Vector.random2D().setMag(
+      random(minVelocityValue, maxVelocityValue)
+    )
+
+    // Configure acceleration and force parameters.
+    // Choose a random color from the color palette passed in.
+    this.acceleration = createVector()
+    this.maxForce = maxForceValue
+    this.maxSpeed = maxSpeedValue
+    this.color = random(hexColorValues)
   }
 
   /**
-   * Renders the boid on the canvas at its current position.
+   * Renders the boids on screen on a cell-by-cell basis,
+   * using the spatial partition created for the boid simulation.
+   *
+   * @param {Boid} boids The array of boids in the same cell.
    */
-  show() {
-    fill(this.color);
-    circle(this.position.x, this.position.y, 8);
-
-    // Loop over each particle and update it.
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      this.particles[i].show();
-    }
+  update(boids) {
+    // Render the boid to the canvas.
+    fill(this.color)
+    circle(this.position.x, this.position.y, this.diameter)
   }
 
   /**
-   * Updates the boid's position and velocity with respect to the
-   * total force accumulated via applying the three rules of flocking.
+   * Enforces separation, alignment, and cohesion with respect to
+   * the boids in the same cell.
+   *
+   * @param {Boid} boids The neighboring boids in the same cell.
    */
-  update() {
-    this.position.add(this.velocity);
-    this.velocity.add(this.acceleration).limit(this.maxSpeed);
-    this.acceleration.set(0, 0);
-
-    // Create a new particle and add it to the list of particles
-    // for this boid. Then loop over each particle and update it.
-    this.timeSinceLastParticle += deltaTime / 1000;
-
-    // if (this.timeSinceLastParticle > this.particlesPerSecond) {
-    this.particles.push(
-      new Particle({
-        position: [this.position.x, this.position.y],
-        velocity: [this.velocity.x, this.velocity.y],
-        color: this.color,
-      })
-    );
-
-    //   this.timeSinceLastParticle = 0;
-    // }
-
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      if (this.particles[i].finished()) {
-        this.particles.splice(i, 1);
-        continue;
-      }
-
-      this.particles[i].update();
-    }
-  }
-
-  /**
-   * Applies the three rules of flocking behaviour to calculate
-   * the direction to move in per frame.
-   */
-  flockWith(boids) {
-    let alignmentForce = this.alignWith(boids).mult(alignmentSlider.value());
-    let cohesionForce = this.cohesionWith(boids).mult(cohesionSlider.value());
-    let separationForce = this.separationWith(boids).mult(
-      separationSlider.value()
-    );
-
-    this.acceleration
-      .add(alignmentForce)
-      .add(cohesionForce)
-      .add(separationForce);
-  }
-
-  /**
-   * Calculates the average velocity of neighboring boids,
-   * and steers towards it.
-   */
-  alignWith(boids) {
-    let perceptionRadius = 50;
-    let steeringForce = createVector();
-    let numNeighbors = 0;
-
-    for (let i = 0; i < boids.length; i++) {
-      let distance = dist(
-        this.position.x,
-        this.position.y,
-        boids[i].position.x,
-        boids[i].position.y
-      );
-
-      if (boids[i] != this && distance < perceptionRadius) {
-        steeringForce.add(boids[i].velocity);
-        numNeighbors++;
-      }
-    }
-
-    if (numNeighbors > 0)
-      steeringForce
-        .div(numNeighbors)
-        .setMag(this.maxSpeed)
-        .sub(this.velocity)
-        .limit(this.maxForce);
-
-    return steeringForce;
-  }
-
-  /**
-   * Calculates the average velocity of neighboring boids,
-   * and steers towards it.
-   */
-  cohesionWith(boids) {
-    let perceptionRadius = 100;
-    let steeringForce = createVector();
-    let numNeighbors = 0;
-
-    for (let i = 0; i < boids.length; i++) {
-      let distance = dist(
-        this.position.x,
-        this.position.y,
-        boids[i].position.x,
-        boids[i].position.y
-      );
-
-      if (boids[i] != this && distance < perceptionRadius) {
-        steeringForce.add(boids[i].position);
-        numNeighbors++;
-      }
-    }
-
-    if (numNeighbors > 0)
-      steeringForce
-        .div(numNeighbors)
-        .sub(this.position)
-        .setMag(this.maxSpeed)
-        .sub(this.velocity)
-        .limit(this.maxForce);
-
-    return steeringForce;
-  }
-
-  /**
-   * Steers away from other boids within a specified protected
-   * radius.
-   */
-  separationWith(boids) {
-    let perceptionRadius = 50;
-    let steeringForce = createVector();
-    let numNeighbors = 0;
-
-    for (let i = 0; i < boids.length; i++) {
-      let distance = dist(
-        this.position.x,
-        this.position.y,
-        boids[i].position.x,
-        boids[i].position.y
-      );
-
-      if (boids[i] != this && distance < perceptionRadius) {
-        let difference = p5.Vector.sub(this.position, boids[i].position).div(
-          distance
-        );
-
-        steeringForce.add(difference);
-        numNeighbors++;
-      }
-    }
-
-    if (numNeighbors > 0)
-      steeringForce
-        .div(numNeighbors)
-        .setMag(this.maxSpeed)
-        .sub(this.velocity)
-        .limit(this.maxForce);
-
-    return steeringForce;
-  }
-
-  /**
-   * Implements screen wrapping to prevent boids from
-   * disappearing from the canvas.
-   */
-  stayInBounds() {
-    if (this.position.x > width / 2) this.position.x = -width / 2;
-    else if (this.position.x < -width / 2) this.position.x = width / 2;
-
-    if (this.position.y > height / 2) this.position.y = -height / 2;
-    else if (this.position.y < -height / 2) this.position.y = height / 2;
-  }
+  _enforceRules(boids) {}
 }
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,41 +1,30 @@
-const boids = [];
-const numBoids = 35;
+// Use these settings to change the screen parameters.
+const view = {
+  width: 1000,
+  height: 800,
+  background: "#FFFFFF",
+}
 
-let overpassMono;
-let alignmentSlider, cohesionSlider, separationSlider;
-
-function preload() {
-  overpassMono = loadFont("../fonts/static/OverpassMono-SemiBold.ttf");
+// Use these settings to change the way boids behave.
+const flock = {
+  numBoids: 50,
+  boidDiameter: 10,
+  spatialHashCellSize: view.height / (boidDiameter * 4),
+  alignment: 0.5,
+  separation: 0.3,
+  cohesion: 0.2,
 }
 
 /**
  * Called by p5.js before the first frame update.
  */
 function setup() {
-  createCanvas(1531, 980, WEBGL);
-
-  // Create sliders that are used to control the parameters
-  // of the simulation.
-  alignmentSlider = createSlider(0, 2, 1, 0.1);
-  cohesionSlider = createSlider(0, 2, 1, 0.1);
-  separationSlider = createSlider(0, 2, 1, 0.1);
-
-  for (let i = 0; i < numBoids; i++) {
-    boids.push(new Boid());
-  }
+  createCanvas(view.width, view.height, WEBGL)
 }
 
 /**
  * Called by p5.js on each frame update.
  */
 function draw() {
-  background(0);
-  noStroke();
-
-  for (let i = 0; i < boids.length; i++) {
-    boids[i].stayInBounds();
-    boids[i].flockWith(boids);
-    boids[i].update();
-    boids[i].show();
-  }
+  background(view.background)
 }
