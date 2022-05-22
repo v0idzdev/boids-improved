38fbef97472df15f1ecd8b9083b315f691119023
Matthew Flegg <matthewflegg@outlook.com>
2022-05-22 15:48:51 +0000

    Implemented spatial hashing

--- scripts/simulation.js
+++ scripts/simulation.js
@@ -1,9 +1,9 @@
 // Spatial hash that contains the boids in the simulation.
 class SpatialHash {
-  constructor(cellSize) {
-    this.cellSize = cellSize
+  constructor({ cellSize: size }) {
+    this.cellSize = size
     this.configureSize()
-    this.createBuckets()
+    this.createNewBuckets()
   }
 
   // Sets the column and row count.
@@ -13,65 +13,107 @@ class SpatialHash {
   }
 
   // Clears all of the buckets and sets their value to an empty array.
-  createBuckets() {
-    this.buckets = {}
+  createNewBuckets() {
+    this.buckets = []
 
-    for (let i = 0; i < this.cols * this.rows; i++) {
-      this.buckets[i] = []
-    }
+    for (let i = 0; i < this.cols * this.rows; i++)
+      this.buckets.push([])
   }
 
   // Registers a boid object with the bucket it should be in.
   registerBoid(boid) {
-    const ids = this.getCellIds(boid)
+    let id = this.getCellId(boid)
 
-    for (let i = 0; i < ids.length; i++) {
-      const id = cellIds[i]
-      this.buckets[id].push(boid)
+    if (id >= 0 && id < this.buckets.length) {
+      if (!this.buckets[id].includes(boid)) 
+        this.buckets[id].push(boid)
+    }
+  }
+
+  // Recalculates cell IDs of all boids and moves them if necessary.
+  updateCells() {
+    let temp = this.buckets
+      .map(x => x.map(x => x))
+      .reduce((a, b) => a.concat(...b), [])
+
+    this.createNewBuckets()
+    
+    for (let i = 0; i < temp.length; i++) {
+      this.registerBoid(temp[i])
     }
   }
 
   // Calculates the IDs of the cells the boid should be in.
-  getCellIds(boid) {
-    let isInBuckets = []
-
-    const min = createVector(boid.pos.x - boid.radius, boid.pos.y - boid.radius)
-    const max = createVector(boid.pos.x - boid.radius, boid.pos.y - boid.radius)
-
-    isInBuckets = this.addBucket(min, isInBuckets) // Top left.
-    isInBuckets = this.addBucket(createVector(max.x, min.y), isInBuckets)
-    isInBuckets = this.addBucket(createVector(max.x, max.y), isInBuckets)
-    isInBuckets = this.addBucket(createVector(min.x, max.y), isInBuckets)
-
-    return isInBuckets
-  }
-
-  // Adds a bucket. Returns a new bucket.
-  addBucket(vec, bucketToLoad) {
-    const cellPosition =
-      floor(vec.x / this.cellSize) + floor(vec.y / this.cellSize) * this.cols
-
-    if (!bucketToLoad.includes(cellPosition)) {
-      bucketToLoad.push(cellPosition)
-    }
-
-    return bucketToLoad
+  getCellId(boid) {
+    return floor(boid.pos.x / this.cellSize) +
+           floor(boid.pos.y / this.cellSize) *
+           this.cols
   }
 
   // Get nearby boids using the spatial hash.
   getNearby(boid) {
-    let boids = []
-    let ids = this.getCellIds(boid)
+    let id = this.getCellId(boid)
 
-    for (let i = 0; i < ids.length; i++) {
-      const id = cellIds[i]
+    return (id < 0 || id >= this.buckets.length)
+      ? []
+      : this.buckets[id] 
+            .map(x => x)
+            .filter(x => x !== boid)
+  }
+}
 
-      for (let j = 0; j < this.buckets[id].length; j++) {
-        boids.push(this.buckets[id][j])
-      }
-    }
+// Models a particle emitted by a boid.
+class Particle {
+  constructor({ pos, vel, colorValue }) {
+    this.pos = pos 
+    this.vel = vel 
+    this.color = this.initColor(colorValue)
 
-    return boids
+    this.alpha = 150
+    this.sizeDissipation = random(0.1, 0.63)
+    this.alphaDissipation = random(0.11, 8.2)
+    this.size = random(1.5, 6.5)
+
+    this.pos = createVector(random(this.pos.x - this.vel.y,
+                                   this.pos.x + this.vel.y),
+                            random(this.pos.y - this.vel.x,
+                                   this.pos.y + this.vel.x))
+  }
+
+  // Chooses a color from colorPalette and linearly
+  // interpolates to it from the emitting boid's color.
+  initColor(colorValue) {
+    let randomColor = random(colorPalette)
+
+    return lerpColor(color(colorValue),
+                     color(randomColor),
+                     deltaTime / 1000)
+  }
+
+  // Updates the particle's transparency and alpha values.
+  update() {
+    this.alpha -= this.alphaDissipation
+    this.size -= this.sizeDissipation
+    this.color.setAlpha(alpha)
+  }
+
+  // Renders the particle on screen.
+  render() {
+    fill(this.color)
+    circle(this.pos.x, this.pos.y, this.size)
+  }
+
+  // Returns a boolean indicating whether to delete the particle.
+  // True if it's invisible or off screen.
+  finished() {
+    return (
+      this.alpha < 0 ||
+      this.size < 0 ||
+      this.pos.x < 0 ||
+      this.pos.y < 0 ||
+      this.pos.x > width ||
+      this.pos.y > height
+    )
   }
 }
 
@@ -87,14 +129,13 @@ class Boid {
     this.radius = radius
     this.maxForce = maxForce
     this.maxSpeed = maxSpeed
-    // ! this.particles = []
+    this.particles = []
   }
 
   // Sets the starting position.
   initPosition() {
-    const x = random(-width, width)
-    const y = random(-height, height)
-
+    const x = random(0, width)
+    const y = random(0, height)
     return createVector(x, y)
   }
 
@@ -109,5 +150,98 @@ class Boid {
   render() {
     fill(this.color)
     circle(this.pos.x, this.pos.y, this.radius * 2)
+  }
+
+  // Updates the boid's position and velociy with respect to others.
+  update(others) {
+    let numNeighbors = 0
+    let force = {
+      alignment: createVector(),
+      cohesion: createVector(),
+      separation: createVector()
+    }
+
+    // Loop over every other boid and update this one.
+    for (let i = 0; i < others.length; i++) {
+      let distance = dist(this.pos.x,
+                          this.pos.y,
+                          others[i].pos.x,
+                          others[i].pos.y)
+      
+      // Calculate forces.
+      force.alignment.add(others[i].vel)
+      force.cohesion.add(others[i].pos)
+
+      let sep = p5.Vector.sub(this.pos, others[i].pos).div(distance)
+      force.separation.add(sep)
+      numNeighbors++
+    }
+
+    // Find the direction to go in to achieve the desired velocity.
+    if (numNeighbors > 0) {
+      force.alignment
+           .div(numNeighbors)
+           .setMag(this.maxSpeed)
+           .sub(this.vel)
+           .limit(this.maxForce)
+           .mult(alignmentSlider.value())
+        
+      force.cohesion
+           .div(numNeighbors)
+           .sub(this.pos)
+           .setMag(this.maxSpeed)
+           .sub(this.vel)
+           .limit(this.maxForce)
+           .mult(cohesionSlider.value())
+        
+      force.separation
+           .div(numNeighbors)
+           .setMag(this.maxSpeed)
+           .sub(this.vel)
+           .limit(this.maxForce)
+           .mult(separationSlider.value())
+    }
+
+    this.pos.add(this.vel)
+    this.vel.add(this.accel).limit(this.maxSpeed)
+
+    // Finally, update acceleration so we can change the boid's
+    // velocity and position.
+    // Reset velocity before adding forces.
+    this.accel.set(0, 0)
+              .add(force.alignment)
+              .add(force.cohesion)
+              .add(force.separation)
+
+    this.screenWrap()
+    this.handleParticles()
+  }
+
+  // Implements screen wrapping so that the boids stay in bounds.
+  screenWrap() {
+    if (this.pos.x + this.radius > width) this.pos.x = this.radius
+    if (this.pos.y + this.radius > height) this.pos.y = this.radius
+    if (this.pos.x - this.radius < 0) this.pos.x = width - this.radius
+    if (this.pos.y - this.radius < 0) this.pos.y = height - this.radius
+  }
+
+  // Handles particles emitted by the boid.
+  // This includes spawning, updating, and rendering.
+  handleParticles() {
+    this.particles.push(new Particle({
+      pos: this.pos,
+      vel: this.vel,
+      colorValue: this.color
+    }))
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      let particle = this.particles[i]
+
+      if (particle.finished())
+        this.particles.splice(i, 1)
+
+      particle.update()
+      particle.render()
+    }
   }
 }
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -2,24 +2,67 @@
 const view = {
   width: 1000,
   height: 800,
-  background: "#FFFFFF",
+  background: "#000000",
 }
 
 // Use these settings to change the way boids behave.
 const flock = {
-  numBoids: 50,
-  boidDiameter: 10,
-  spatialHashCellSize: view.height / (boidDiameter * 4),
+  numBoids: 250,
+  boidDiameter: 8,
+  spatialHashCellSize: 50,
   alignment: 0.5,
   separation: 0.3,
   cohesion: 0.2,
+  initialVelocity: { min: 2, max: 4 },
 }
+
+// Boids will be one of these colors, chosen at random.
+const colorPalette = [
+  "#FB8C0D",
+  "#E35E0B", 
+  "#FA3A00",
+  "#E31D0B",
+  "#FF1A4E"
+]
+
+// This will be used to store the boids.
+let boids
+
+// These will be used to control parameters while the simulation
+// is running
+let cohesionSlider
+let alignmentSlider
+let separationSlider
 
 /**
  * Called by p5.js before the first frame update.
  */
 function setup() {
-  createCanvas(view.width, view.height, WEBGL)
+  createCanvas(view.width, view.height)
+  background(view.background)
+
+  boids = new SpatialHash({ cellSize: flock.spatialHashCellSize })
+
+  // These are set to optimal default values.
+  // These can be changed for debugging purposes if needed.
+  alignmentSlider = createSlider(0, 2, 0.5, 0.1);
+  cohesionSlider = createSlider(0, 2, 0.2, 0.1);
+  separationSlider = createSlider(0, 2, 0.3, 0.1);
+
+  // Generate boids based on the parameters set above.
+  for (let i = 0; i < flock.numBoids; i++) {
+    boids.registerBoid(new Boid({
+      velocity: [
+        flock.initialVelocity.min,
+        flock.initialVelocity.max
+      ],
+
+      colors: colorPalette,
+      radius: flock.boidDiameter / 2,
+      maxForce: 0.3,
+      maxSpeed: 4,
+    }))
+  }
 }
 
 /**
@@ -27,4 +70,21 @@ function setup() {
  */
 function draw() {
   background(view.background)
+  noStroke()
+
+  // Loop over each boid in each cell and update it with respect to
+  // the other boids in its cell.
+  for (let i = 0; i < boids.buckets.length; i++) {
+    let bucket = boids.buckets[i]
+
+    for (let j = 0; j < bucket.length; j++) {
+      let boid = bucket[j]
+
+      boid.update(boids.getNearby(boid))
+      boid.render()
+    }
+  }
+
+  // Clear buckets and calculate new cell positions.
+  boids.updateCells()
 }
